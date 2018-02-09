8f9b436fb699e60688048e021c569267493619f8
katherinelef <katherine.lef@hotmail.com>
2018-02-09 08:17:34 -0500

    añadiendole funcionalidad trazar ruta

--- js/app.js
+++ js/app.js
@@ -1,4 +1,75 @@
-// La funcionalidad de tu proyecto
+// trazando ruta con api google map
+function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 15
+        });
+
+        var infoWindow = new google.maps.InfoWindow({map: map});
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+
+        var inputPartida = document.getElementById('inputPartida');
+        var inputDestino = document.getElementById('inputDestino');
+        new google.maps.places.Autocomplete(inputPartida);
+        new google.maps.places.Autocomplete(inputDestino);
+
+        var directionService = new google.maps.DirectionsService;
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+
+        var calculateAndDisplayRoute = function (directionService,directionsDisplay){
+          directionService.route({
+            origin:inputPartida.value,
+            destination:inputDestino.value,
+            travelMode:'DRIVING'
+          }, function (response,status){
+            if(status==='OK'){
+              directionsDisplay.setDirections(response);
+            } else {
+              window.alert('No encontramos una ruta.');
+            }
+          }
+        )};
+
+        directionsDisplay.setMap(map);
+        var trazarRuta = function(){
+          calculateAndDisplayRoute(directionService,directionsDisplay);
+        };
+        document.getElementById('trazar-ruta').addEventListener('click',trazarRuta);
+      }
+
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+      }
+
+
+
+
+
+
+
+// Modales
 $('#exampleModal').on('show.bs.modal', function (event) {
   var button = $(event.relatedTarget) // Button that triggered the modal
   var recipient = button.data('whatever') // Extract info from data-* attributes
